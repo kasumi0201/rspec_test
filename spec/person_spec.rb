4672cc6557f81908dc61699091322da52265739b
kasumi <kasumi0201@gmail.com>
2017-11-20 15:23:03 +1100

    second

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
-describe Person do
-  let(:bob) { Person.new('bob') }
+# describe Person do
+#    let(:person1) { Person.new('kasumi','takeshima') }
+#
+#   describe '.hello' do
+#     it { expect(person1.hello).to eq 'Hello, my name is kasumi takeshima.'}
+#   end
+# end
 
+
+describe Person do
   describe '.hello' do
-    it { expect(bob.hello).to eq 'Hello, my name is bob.'}
+    it 'a' do
+      person = Person.new("kasumi","takeshima")
+      expect(person.hello).to eq 'Hello, my name is kasumi takeshima.'
+    end
   end
 end
--- src/person.rb
+++ src/person.rb
@@ -1,11 +1,15 @@
 require_relative 'libs/message'
 
 class Person
-  def initialize(name)
-    @name = name
+  def initialize(firstname,lastname)
+    @firstname = firstname
+    @lastname = lastname
   end
 
   def hello
-    "#{Message.hello}, my name is #{@name}."
+     "#{Message.hello}, my name is #{@firstname} #{@lastname}."
   end
 end
+
+ person = Person.new("kasumi","takeshima")
+puts  person.hello
